434fc1d2a3eef8c284663f8e42d645d8bf3c522d
appersoncory <84872642+appersoncory@users.noreply.github.com>
2024-06-18 15:29:54 -0400

    Update db-connector.js

--- database/db-connector.js
+++ database/db-connector.js
@@ -6,10 +6,10 @@ var mysql = require('mysql')
 // Create a 'connection pool' using the provided credentials
 var pool = mysql.createPool({
     connectionLimit : 10,
-    host            : 'classmysql.engr.oregonstate.edu',
-    user            : 'cs340_phanand',
-    password        : '6275',
-    database        : 'cs340_phanand'
+    host            : '',
+    user            : '',
+    password        : '',
+    database        : ''
 })
 
 // Export it for use in our applicaiton
